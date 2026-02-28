0c1c552ffb1360f5d229c8abafc1ad95ee77faa5
Shenal Sanjana <shenal.sanjana.16@gmail.com>
2026-02-28 23:03:38 +0530

    develop:   - done: initial django ninja framwork and dependancies installed.   - done: data base settings updated.

--- core/settings.py
+++ core/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from pyexpat.errors import messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +36,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd party
+    'taggit',
+    'import_export',
+    'corsheaders',
+    'rest_framework',
+    'ninja',
+    'ninja_jwt',
+    'ninja_extra',
+    'django_ckeditor_5',
+
+    # services
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +59,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -68,7 +81,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
@@ -84,7 +96,7 @@ DATABASES = {
         },
         'NAME': 'db_v1',
         'USER': 'postgres',
-        'PASSWORD': 'Dpc@Scotland21#',
+        'PASSWORD': '1996',
         'HOST': 'localhost',
         'PORT': '5432'
     },
@@ -95,12 +107,11 @@ DATABASES = {
         },
         'NAME': 'db_v1',
         'USER': 'postgres',
-        'PASSWORD': 'Dpc@Scotland21#',
+        'PASSWORD': '1996',
         'HOST': 'localhost',
         'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -120,7 +131,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
@@ -131,7 +141,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
